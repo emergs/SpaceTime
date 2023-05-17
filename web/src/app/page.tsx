3699a61481362f31b6da65fbe22ea198804389d4
Emerson Gonçalves dos Santos <emer_gs@hotmail.com>
2023-05-17 04:30:38 +0000

    feat:initial settings front-end and back-end

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screnn bg-zinc-950 text-zinc-50 p-6">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+  );
+}
